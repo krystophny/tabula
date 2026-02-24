c61326f564ec8b2cf2ede62512e66717da294b89
Christopher Albert <albert@tugraz.at>
2026-02-24 13:24:33 +0100

    chore: release bump metadata to v0.1.2 and add release notes

--- internal/appserver/client.go
+++ internal/appserver/client.go
@@ -154,7 +154,7 @@ func (c *Client) SendPromptStream(ctx context.Context, req PromptRequest, onEven
 			"clientInfo": map[string]interface{}{
 				"name":    "tabura-web",
 				"title":   "Tabura Web",
-				"version": "0.1.1-dev",
+				"version": "0.1.2",
 			},
 			"capabilities": map[string]interface{}{
 				"experimentalApi": true,
